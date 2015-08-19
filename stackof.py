cffe7300ea8ccc208ac5fa50df8d9a4a753c3f3a
slg916 <legong.sun@gmail.com>
2015-08-20 00:56:14 +0900

    All blocks translation finished

--- stackof.py
+++ stackof.py
@@ -42,6 +42,23 @@ def filter_link_list(link_list, visited):
             valid_list.append(link)
     return valid_list
 
+def grep_title(url):
+    title_dash = url.split('/')[-1]
+    title = title_dash.replace('-', ' ')
+    return title
+
+def title_trans(title_en, keep_en):
+    gs = goslate.Goslate()
+    title_zh = gs.translate(title_en, 'zh')
+    title_html = ''
+    if keep_en:
+        title_zh_h1 = str.format("<h1>{0}</h1>", title_zh)
+        title_en_h2 = str.format("<h2>{0}</h2>", title_en)
+        title_html = title_zh_h1 + title_en_h2
+    else:
+        title_html = str.format("<h1>{0}</h1>", title_zh)
+    return title_html
+
 def grep_question(url):
     response = requests.get(url)
     html_str = response.text
@@ -72,37 +89,66 @@ def grep_answers(url):
         answer_list.append(per_ans)
     return answer_list
 
-def html_trans(html_en):
+def html_trans(html_en, keep_en):
     gs = goslate.Goslate()
+
+    # html 2 soup
     soup = bs4.BeautifulSoup(html_en, "html.parser")
+
+    # save all code blocks
     code_list = soup.find_all('code')
 
+    # drop tags of html, just save pure text
     text_en = soup.text
+
+    # replace all code blocks to non-sense text
     for code in code_list:
         text_en = text_en.replace(code.text, "edocsiereh")
 
+    # trans en to zh for pure text
     text_zh = gs.translate(text_en, 'zh')
+
+    # split en pure text by non-sense text used above
     text_zh = text_zh.split("edocsiereh")
+
+    # split zh pure text by non-sense text used above
     text_en = text_en.split("edocsiereh")
 
+    # recover pure text to html again
     for i,zh in enumerate(text_zh):
-        en = text_en[i]
-        text_zh[i] = str.format("<p>{0}<br>{1}</p>", en, zh)
+        if keep_en:
+            en = text_en[i]
+            text_zh[i] = str.format("<p>{0}<br>{1}</p>", en, zh)
+        else:
+            text_zh[i] = str.format("<p>{0}</p>", zh)
 
+    # add <pre> tag before <code>, otherwise code be shown strange
     for i,code in enumerate(code_list):
         code_list[i] = str.format("<pre>{0}</pre>", code)
 
+    # insert code into text html
     for i,code in enumerate(code_list):
         text_zh.insert(i*2+1, code)
 
+    # change list into just one long string
     html_zh = ""
     for text in text_zh:
         html_zh += str(text)
 
+    # change new zh html string into soup
     soup_zh = bs4.BeautifulSoup(html_zh, "html.parser")
-    print(soup_zh.prettify(formatter="html"))
+
+    # format the html
+    html_zh = soup_zh.prettify(formatter="html")
 
     return html_zh
+
+def html_list_trans(html_list_en):
+    html_list_zh = []
+    for html_en in html_list_en:
+        html_zh = html_trans(html_en, False)
+        html_list_zh.append(html_zh)
+    return html_list_zh
 
 def part_to_full_html(part):
     soup = bs4.BeautifulSoup(part, "html.parser")
@@ -147,19 +193,29 @@ for page, node_url in enumerate(page_list):
         visited |= {url}  # 标记为已访问
         #  print(str.format("正在抓取第{0}页/第{1}题\t <--- {2}", page+1, cnt, url))
 
-        url = "http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array"
+        #  url = "http://stackoverflow.com/questions/11227809/why-is-processing-a-sorted-array-faster-than-an-unsorted-array"
 
-        # 解析问题页面的html
-        question = grep_question(url)
+        # Grep En Info
+        title_en = grep_title(url)
+        answer_list_en = grep_answers(url)
+        question_html_en = grep_question(url)
+
+        # Grep Question Tags List
         tag_list = grep_tags(url)
-        answer_list = grep_answers(url)
-        #  print(question)
-        #  print(tag_list)
-        #  print('---------------------------------------\n')
 
-        question_zh = html_trans(question)
-        #  print(question_zh)
+        # En to Zh Translation
+        title_html_zh = title_trans(title_en, True)
+        question_html_zh = html_trans(question_html_en, False)
+        answer_list_zh = html_list_trans(answer_list_en)
 
-        break
+        print(title_html_zh)
+        print('Tags:', tag_list)
+        print(question_html_zh)
+        for i,answer in enumerate(answer_list_zh):
+            print('Answer', i, '----------------------------------------------------------------')
+            print(answer)
+
+        if cnt > 2:
+            break
 
 #  print("抓取结束。")
